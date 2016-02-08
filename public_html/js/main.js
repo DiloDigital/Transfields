e880a7001cbedd02e95afe73308dc004b2d602cd
alex_ss <alexis.bm69@gmail.com>
2016-02-08 14:10:58 -0300

    seccion pagook

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,58 @@
+// Aqui esta el formulario de contactanos
+
+// Get the form.
+    var form = $('#ajax-contact');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+
+        // TODO
+
+        // Serialize the form data.
+        var formData = $(form).serialize();
+
+        // Submit the form using AJAX.
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        })
+
+        .done(function(response) {
+            // Make sure that the formMessages div has the 'success' class.
+            $(formMessages).removeClass('error');
+            $(formMessages).addClass('success');
+
+            // Set the message text.
+            $(formMessages).text(response);
+
+            // Clear the form.
+            $('#nombre').val('');
+            $('#correo').val('');
+            $('#telefono').val('');
+            $('#mensaje').val('');
+        })
+
+        .fail(function(data) {
+            // Make sure that the formMessages div has the 'error' class.
+            $(formMessages).removeClass('success');
+            $(formMessages).addClass('error');
+
+            // Set the message text.
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('¡Huy! Se produjo un error y su mensaje no se pudo enviar.');
+            }
+        });
+
+
+
+    });
+
 
---
+++ public_html/mailer.php
@@ -0,0 +1,54 @@
+<?php
+
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["nombre"]));
+                $name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["correo"]), FILTER_SANITIZE_EMAIL);
+        $telefono = trim($_POST["telefono"]);
+        $message = trim($_POST["mensaje"]);
+
+        // Check that data was sent to the mailer.
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "¡Huy! Hubo un problema con el envío. Por favor complete el formulario y vuelva a intentarlo.";
+            exit;
+        }
+
+        // Set the recipient email address.
+        // FIXME: Update this to your desired email address.
+        $recipient = "alexis.berrios69@gmail.com";
+
+
+        // Set the email subject.
+        $subject = "Formulario de contacto por : $name";
+
+        // Build the email content.
+        $email_content = "Nombre:  $name\n";
+        $email_content .= "Correo Electrónico:  $email\n";
+        $email_content .= "Teléfono: $telefono\n";
+        $email_content .= "Mensaje: $message\n";
+
+        // Build the email headers.
+        $email_headers = "De: $name <$email>";
+
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "¡Gracias! Tu mensaje ha sido enviado.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "¡Huy! Algo salió mal y no pudimos enviar tu mensaje.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "Hubo un problema con el envío, por favor intente de nuevo.";
+    }
+
+?>
