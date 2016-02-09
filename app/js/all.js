32d9370a9c81125eebaaa6fc8ca0cffbc44f77d4
alex_ss <alexis.bm69@gmail.com>
2016-02-09 00:46:46 -0300

    scrollTo en botones ok

--- app/js/all.js
+++ app/js/all.js
@@ -1,29 +1,3 @@
-$(function(){
-// ScrollTo contactanos
-     $('a[href*=#]').click(function() {
-
-     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-         && location.hostname == this.hostname) {
-
-             var $target = $(this.hash);
-
-             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-
-             if ($target.length) {
-
-                 var targetOffset = $target.offset().top;
-
-                 $('html,body').animate({scrollTop: targetOffset}, 1000);
-
-                 return false;
-
-            }
-
-       }
-
-   });
-
-
 // Get the form.
     var form = $('#ajax-contact');
 
@@ -76,4 +50,3 @@ $(function(){
         });
 
     });
-});
